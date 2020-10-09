9790c0b681bbdcbee2d2b2ac00d9bedc44738625
Nicolas Tomlinson-StJuste <ntom@sas.upenn.edu>
2020-10-08 20:03:13 -0400

    Basic stuff

--- script.js
+++ script.js
@@ -43,3 +43,15 @@ $.ajax({
       console.log(response);
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+        
